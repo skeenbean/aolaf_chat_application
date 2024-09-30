80fa413a922845004596af0a41fe7436b72ddc43
AyomideO <mideolafusi@gmail.com>
2024-09-30 08:41:18 -0400

    RealTime Chat Application (progress)

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '64bc5883-b594-4a4c-b7c9-47396c2d683e';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
+};
+
+export default App;
